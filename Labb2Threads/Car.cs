fa61d9b6bcdc6a319814c2fecd520673e57c1f09
ordio <max@maxsamuelsson.se>
2023-12-17 19:25:27 +0100

    Add project files.

---
+++ Labb2Threads/Car.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Labb2Threads
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Distance { get; set; }
+        public int EventTimer { get; set; }
+
+        public Car(string name, int speed, int distance)
+        {
+            Name = name;
+            Speed = 100; // 100km/h
+            Distance = distance;
+            EventTimer = 0;
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    CheckCarStatus();
+                    await Task.Delay(1000);
+                    Distance += Speed;
+                    EventTimer++;
+                    if (EventTimer == 30) 
+                    {
+                        await ApplyEvents(this);
+                        EventTimer = 0;
+                    }
+                }
+            });
+        }
+
+        public void CheckCarStatus()
+        {
+            Console.WriteLine($"{Name} status: Distance = {Distance}, Speed = {Speed} km/h");
+        }
+
+        public static async Task ApplyEvents(Car car)
+        {
+            Random random = new Random();
+            int randomNum = random.Next(1, 51);
+
+            if (randomNum == 1) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Console.Out.WriteLineAsync($"!!!!{car.Name} har slut på bensin! Stannar: 30s");
+                Console.ForegroundColor = ConsoleColor.White;
+                await Task.Delay(30000);
+            }
+            else if (randomNum <=3 && randomNum != 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Console.Out.WriteLineAsync($"!!!!{car.Name} fick punktering! Stannar: 20s");
+                Console.ForegroundColor = ConsoleColor.White;
+                await Task.Delay(20000);
+            }
+            else if (randomNum <= 8 && randomNum > 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Console.Out.WriteLineAsync($"!!!!{car.Name} har en fågel på vindrutan! Stannar: 10s");
+                Console.ForegroundColor = ConsoleColor.White;
+                await Task.Delay(10000);
+            }
+            else if (randomNum <= 18 && randomNum > 8)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                await Console.Out.WriteLineAsync($"!!!!{car.Name} har fått motorfel! Sänker hastighet med 1km/h");
+                Console.ForegroundColor = ConsoleColor.White;
+                car.Speed --;
+            }
+        }
+    }
+}
---
+++ Labb2Threads/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Labb2Threads
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Car car1 = new Car("Audi", 100, 0);
+            Car car2 = new Car("Tesla", 100, 0);
+
+            Task car1Task = RunCar(car1);
+            Task car2Task = RunCar(car2);
+
+            await Task.WhenAll(car1Task, car2Task);
+
+            if (car1.Distance > car2.Distance)
+            {
+                Console.Out.WriteLineAsync($"{car1.Name} won");
+            }
+            else if (car1.Distance < car2.Distance)
+            {
+                Console.Out.WriteLineAsync($"{car2.Name} won");
+            }
+        }
+
+        static async Task RunCar(Car car)
+        {
+            int finishSpeed = 0;
+            int raceDistance = 10000;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{car.Name} has started driving");
+            bool raceActive = true;
+
+            while (raceActive)
+            {
+                finishSpeed++;
+
+                await Task.Delay(1000);
+
+                CheckEnterKey(car);
+
+                if (car.Distance >= raceDistance)
+                {
+                    raceActive = false;
+                    Console.WriteLine($"{car.Name} Has finished the race in {finishSpeed} Seconds");
+                }
+            }
+            
+        }
+
+        static void CheckEnterKey(Car car)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    car.CheckCarStatus();
+                }
+            }
+        }
+    }
+}
